efeaaac95a8d1d4071b79950ddd1528373cde5c1
SebbeKYH <sebastian.andersson2@student.kyh.se>
2022-03-24 19:20:05 +0100

    still working on encryption

--- controllers/message_controller.py
+++ controllers/message_controller.py
@@ -10,18 +10,20 @@ def create_message(title, body, receiver_id):
     from models import Message
     # User is the current logged in user
     user = current_user
-
-    message = Message(title=title, body=body, sender_id=user.id)
-    aes_key, aes_cipher, aes_nonce, aes_tag = aes_encrypt(message)
-    # CREATE REFERENCE TO KEY NAME
-
+    # Message will contain title body and sender_id
+    message = Message(aes_key=rsa_aes_combo, body=body, sender_id=user.id)
     receiver_id = int(receiver_id)
     receiver = get_user_by_id(receiver_id)
+    message.receivers.append(receiver)
+    # body of message will be aes encrypted...
+    combo = encrypt(message=body, reciever_email=receiver.email)
+    print()
 
     #recipient_rsa_key_name = get_user_by_email(user_email=User.email)
-    encrypted_aes_key = rsa_encrypt(key_name=receiver.email, message=aes_key)
+
+
+
     #encoded = base64.b64encode(encrypted_aes_key, 'utf-8')
-    message.receivers.append(receiver)
     from app import db
 
     db.session.add(message)
@@ -40,13 +42,18 @@ def get_unread_msg_count():
             msg_count += 1
     return msg_count
 
-def aes_encrypt(message):
+def encrypt(reciever_email, message):
     key = get_random_bytes(16)
     cipher_aes = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher_aes.encrypt_and_digest(message.encode('utf-8'))
-    return key, ciphertext, cipher_aes.nonce, tag
 
-def rsa_encrypt(key_name, message):
-    recipient_key = RSA.importKey(open(f'./keys/{key_name}_public.pem').read())
+    recipient_key = RSA.importKey(open(f'./keys/{reciever_email}_public.pem').read())
     cipher_rsa = PKCS1_OAEP.new(recipient_key)
-    return cipher_rsa.encrypt(message)
+    rsa_aes_combo = cipher_rsa.encrypt(key)
+
+    return rsa_aes_combo
+    #return key, ciphertext, cipher_aes.nonce, tag
+
+
+#def rsa_encrypt(key_name, key):
+#    return cipher_rsa.encrypt(key)
--- models.py
+++ models.py
@@ -39,6 +39,7 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(250))
     body = db.Column(db.Text)
+    aes_key = db.Column(db.Text)
     read = db.Column(db.BOOLEAN, default=False)
     sent_time = db.Column(db.DateTime, default=datetime.datetime.now())
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
